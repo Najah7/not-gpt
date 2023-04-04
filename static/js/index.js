e765c7270e7b15f1c49e198a516092ebce1fbc00
Najah7 <najahmatsuo7@gmail.com>
2023-04-04 21:04:21 +0900

    ✨ ボタンを押したら、imgタグのGIFが変更するJSを追加

--- static/js/index.js
+++ static/js/index.js
@@ -4,3 +4,15 @@ textarea.addEventListener('input', () => {
   textarea.style.height = 'auto';
   textarea.style.height = textarea.scrollHeight + 'px';
 });
+
+// judge-button要素を取得
+const judgeButton = document.querySelector('.judge-button');
+
+// judge-buttonがクリックされた時の処理を定義
+judgeButton.addEventListener('click', () => {
+  // img要素を取得
+  const gptImg = document.querySelector('.gpt-img');
+
+  // img要素のsrc属性を変更
+  gptImg.src = '../static/image/judging.gif';
+});
